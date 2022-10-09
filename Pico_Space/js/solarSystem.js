a896f29d032063a5a6e3e82a157ce1040a27204a
fredrikvalset <53392485+fredrikvalset@users.noreply.github.com>
2022-10-09 19:55:38 +0200

    Add files via upload

--- Pico_Space/js/solarSystem.js
+++ Pico_Space/js/solarSystem.js
@@ -1,26 +1,30 @@
-"use strict";
+'use strict';
 
 import {
-    AmbientLight, Color,
-    Mesh,
-    MeshBasicMaterial,
-    MeshPhongMaterial, MeshPhysicalMaterial,
-    Object3D, PointLight, Sphere,
-    SphereGeometry, Texture,
-    TextureLoader
-} from "./build/three.module.js";
-import SimpleColorMaterial from "./SimpleColorMaterial.js";
+  AmbientLight,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  MeshPhongMaterial,
+  MeshPhysicalMaterial,
+  Object3D,
+  PointLight,
+  Sphere,
+  SphereGeometry,
+  Texture,
+  TextureLoader,
+} from './build/three.module.js';
+import SimpleColorMaterial from './simpleColorMaterial.js';
 
 export default class solarSystem {
-
-constructor(scene) {
+  constructor(scene) {
     let radius = 5;
     let widthSegments = 64;
     let heightSegment = 64;
 
     let sunGeometry = new SphereGeometry(radius, widthSegments, heightSegment);
 
-    let sunTextureUrl = "assets/texture_sun.jpg";
+    let sunTextureUrl = 'assets/texture_sun.jpg';
     let sunTexture = new TextureLoader().load(sunTextureUrl);
 
     /*let sunMaterial = new MeshBasicMaterial({
@@ -28,9 +32,9 @@ constructor(scene) {
     });*/
 
     let sunMaterial = new SimpleColorMaterial({
-        mapInParameters: sunTexture,
-        colorInParameters: new Color(0xFF0000)
-    })
+      mapInParameters: sunTexture,
+      colorInParameters: new Color(0xff0000),
+    });
 
     this.sun = new Mesh(sunGeometry, sunMaterial);
     this.sun.position.z = -50;
@@ -42,31 +46,32 @@ constructor(scene) {
     this.earthOrbitNode = new Object3D();
     this.sun.add(this.earthOrbitNode);
 
-    let earthTextureUrl = "assets/texture_earth.jpg";
+    let earthTextureUrl = 'assets/texture_earth.jpg';
     let earthTexture = new TextureLoader().load(earthTextureUrl);
 
+    let earthSpecularMap = new TextureLoader().load('assets/earthspec1k.jpg');
+    let earthNormalMap = new TextureLoader().load(
+      'assets/2k_earth_normal_map.png'
+    );
 
-    let earthSpecularMap = new TextureLoader().load("assets/earthspec1k.jpg");
-    let earthNormalMap = new TextureLoader().load("assets/2k_earth_normal_map.png");
-
-
-    let earthMaterial = new MeshPhongMaterial({map:earthTexture,
-        shininess:1.0,
-        specular: earthSpecularMap,
-        normalMap: earthNormalMap
+    let earthMaterial = new MeshPhongMaterial({
+      map: earthTexture,
+      shininess: 1.0,
+      specular: earthSpecularMap,
+      normalMap: earthNormalMap,
     });
-     
-
 
     radius = 2.5;
-    let earthGeometry = new SphereGeometry(radius, widthSegments, heightSegment);
+    let earthGeometry = new SphereGeometry(
+      radius,
+      widthSegments,
+      heightSegment
+    );
 
     this.earth = new Mesh(earthGeometry, earthMaterial);
 
     this.earth.position.x = 15;
     this.earthOrbitNode.add(this.earth);
-
-
 
     // Mars
 
@@ -76,18 +81,20 @@ constructor(scene) {
     // Legger det til som et barn av solen
     this.sun.add(this.marsOrbitNode);
 
-    let marsTextureUrl = "assets/mars_1k_color.jpg";
+    let marsTextureUrl = 'assets/mars_1k_color.jpg';
     let marsTexture = new TextureLoader().load(marsTextureUrl);
 
-    let marsSpecularMap = new TextureLoader().load("assets/earthspec1k.jpg");
-    let marsNormalMap = new TextureLoader().load("assets/2k_earth_normal_map.png");
+    let marsSpecularMap = new TextureLoader().load('assets/earthspec1k.jpg');
+    let marsNormalMap = new TextureLoader().load(
+      'assets/2k_earth_normal_map.png'
+    );
 
     let marsMaterial = new MeshPhongMaterial({
-        map: marsTexture,
-        shininess: 1.0,
-        specular: marsSpecularMap,
-        normalMap: marsNormalMap
-    })
+      map: marsTexture,
+      shininess: 1.0,
+      specular: marsSpecularMap,
+      normalMap: marsNormalMap,
+    });
 
     radius = 3.2;
 
@@ -99,7 +106,6 @@ constructor(scene) {
     // Gjør Mars til et barn av marsOrbitNode, arver rotasjonen som gjøres på marsOrbitNode
     this.marsOrbitNode.add(this.mars);
 
-
     // Mercury
 
     this.mercuryOrbitNode = new Object3D();
@@ -107,29 +113,34 @@ constructor(scene) {
     // Legger det til som et barn av solen
     this.sun.add(this.mercuryOrbitNode);
 
-    let mercuryTextureUrl = "assets/mercurymap.jpg";
+    let mercuryTextureUrl = 'assets/mercurymap.jpg';
     let mercuryTexture = new TextureLoader().load(mercuryTextureUrl);
 
-    let mercurySpecularMap = new TextureLoader().load("assets/earthspec1k.jpg");
-    let mercuryNormalMap = new TextureLoader().load("assets/2k_earth_normal_map.png");
+    let mercurySpecularMap = new TextureLoader().load('assets/earthspec1k.jpg');
+    let mercuryNormalMap = new TextureLoader().load(
+      'assets/2k_earth_normal_map.png'
+    );
 
     let mercuryMaterial = new MeshPhongMaterial({
-        map: mercuryTexture,
-        shininess: 1.0,
-        specular: mercurySpecularMap,
-        normalMap: mercuryNormalMap
-    })
+      map: mercuryTexture,
+      shininess: 1.0,
+      specular: mercurySpecularMap,
+      normalMap: mercuryNormalMap,
+    });
 
     radius = 1;
 
-    let mercuryGeometry = new SphereGeometry(radius, widthSegments, heightSegment);
+    let mercuryGeometry = new SphereGeometry(
+      radius,
+      widthSegments,
+      heightSegment
+    );
     this.mercury = new Mesh(mercuryGeometry, mercuryMaterial);
     this.mercury.position.x = -10;
     this.mercury.position.z = 5;
 
     // Gjør Mercury til et barn av mercuryOrbitNode, arver rotasjonen som gjøres på mercuryOrbitNode
     this.marsOrbitNode.add(this.mercury);
-
 
     // PointLight, lyser i alle retninger
     this.sunLight = new PointLight(0xffffff, 3);
@@ -139,13 +150,9 @@ constructor(scene) {
     // Bakgrunnsbelysning
     this.ambiantLight = new AmbientLight(0xffffff, 0.05);
     scene.add(this.ambiantLight);
+  }
 
-    
-
-}
-
-
-animate() {
+  animate() {
     this.sun.rotation.y += 0.005;
 
     this.earthOrbitNode.rotation.y += 0.01;
@@ -156,8 +163,5 @@ animate() {
 
     this.mercuryOrbitNode.rotation.y += 0.05;
     this.mercury.rotation.y += 0.01;
-
-
-}
-
+  }
 }
